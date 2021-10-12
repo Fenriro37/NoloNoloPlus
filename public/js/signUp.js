da626ac7a4aaadc5027feb39bb70634bdd8ef8d6
Han Chu <hanchu@MBP-di-Han.homenet.telecomitalia.it>
2021-10-12 21:01:19 +0200

    Update sign-up - complete the form

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,14 +1,19 @@
 window.addEventListener("load", () => {
     // Seleziona il bottone per inviare il form
-    $("#invia").click((event) => event.preventDefault());
-    $("#invia").click(async () => {
+    $("#signUp").click((event) => event.preventDefault());
+    $("#signUp").click(async () => {
         // Cattura dei dati
         const data = []
         $(".card input").each(function() {
             data.push($(this).val())
         });
+        $(".card select").each(function() {
+            data.push($(this).val())
+        });
+        const birthday = data[2].split("-");
         // Controllo dei dati con una stampa
         console.log(data)
+        console.log(birthday)
         // Invio dei dati
         $.ajax({
             url: "/signUp",
@@ -17,11 +22,29 @@ window.addEventListener("load", () => {
                 "Content-Type": "application/json"
             },
             data: JSON.stringify({
-                user: data[0],
-                email: data[2],
-                password: data[3],
-                address: data[1],
-                payment: data[4]
+                userName: data[0],
+                userSurname: data[1],
+                birthday: {
+                    year: birthday[0],
+                    month: birthday[1],
+                    day: birthday[2]
+                },
+                phone: data[3],
+                email: data[4],
+                password: data[5],
+                address: {
+                    addressStreet: data[6],
+                    addressNumber: data[7],
+                    addressCity: data[8]
+                },
+                paymentMethod: {
+                    cardType: data[14],
+                    cardName: data[9],
+                    cardSurname: data[10],
+                    cardExpireMonth: data[15],
+                    cardExpireYear: data[16],
+                    cardCCV: data[11]
+                }
             }),
             // Risposta del server in caso di successo
             success: () => {
