99367a885c2c52c26fb98e67c31e74c017fc8001
Fenrir37 <devilmatti00@gmail.com>
2021-12-21 13:02:00 +0100

    inizio funzionario

--- server/frontEnd/worker/js/myScript.js
+++ server/frontEnd/worker/js/myScript.js
@@ -1,6 +1,36 @@
-import Functions from './functions/function'
 
-document.addEventListener('DOMContentLoaded', function() {
-    Functions.loginAsWorker()
-    console.log("suca")
-}, false);
+ function login() {
+  console.log("cacca")
+  $.ajax({
+      url: "/api/public/login",
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      data: JSON.stringify({
+          email: "han.chu@worker.com",
+          plainTextPassword: "1234567890"
+      }),
+      // Risposta del server in caso di successo
+      success: (result) => {
+          console.log(result)
+          document.cookie = 'jwt=' + result.data;
+          //window.location.href = "http://localhost:8081/user/index.html";
+      },
+      // Risposta del server in caso di insuccesso
+      error: (error) => {
+          console.log("Error");
+          alert("Errore. " + error.responseText);
+      }
+  });
+}
+
+/* let result = http.post(
+    "/api/public/login",
+    {
+        email: "han.chu@worker.com",
+        plainTextPassword: "1234567890"
+    }
+);
+return result;
+} */
