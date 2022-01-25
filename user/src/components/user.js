5457b16a07c3cf22a7956e16bf97a20c107cbf80
Jean-cube <gianpaolo.roversi@studio.unibo.it>
2022-01-25 10:34:05 +0100

    onCommit

--- user/src/components/user.js
+++ user/src/components/user.js
@@ -51,7 +51,8 @@ export class User extends Component {
       ],      
     }
     this.switchModifying = this.switchModifying.bind(this);
-    this.save = this.save.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
@@ -91,7 +92,7 @@ export class User extends Component {
     this.setState({boolModifying: boolModifying});
   }
 
-  save = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
     console.log("inizio save: " + this.state.boolModifying);
     if (this.state.boolModifying) {
@@ -100,28 +101,24 @@ export class User extends Component {
       //  alert('password troppo debole');
       //  return null;
       //}
-      
-
-      console.log('event target-------');
-      console.log(event.target);
 
       //Controllo Input non vuoti
-      for(var index in event.target){
-        console.log(index);
-        console.log(event.target[index]);
-        if(event.target[index] == ''){
-          alert('Non hai compilato tutti i campi.');
-          return null;
-        }
-      }
+      //for(var index in event.target){
+      //  console.log(index);
+      //  console.log(event.target[index]);
+      //  if(event.target[index] == ''){
+      //    alert('Non hai compilato tutti i campi.');
+      //    return null;
+      //  }
+      //}
       
       //Post API
-      const query = {
-        userName: event.target[0].value,
-        userSurname: event.target[1].value,
-      }
-
-      console.log(query);
+      //const query = {
+      //  userName: event.target[0].value,
+      //  userSurname: event.target[1].value,
+      //}
+//
+      //console.log(query);
 
       //apiCall.postUser(null, query).then(() => {
       //    alert('success');
@@ -132,7 +129,10 @@ export class User extends Component {
     this.switchModifying();
     console.log("switch: " + this.state.boolModifying);
   }
-  
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
   render() {
     return (
@@ -147,25 +147,26 @@ export class User extends Component {
             <h1>Personal Page</h1>
           </Col>
         </Row>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <fieldset disabled={!this.state.boolModifying} >
-            <Form.Group className="mb-2" controlId="formBasicText">
+            <Form.Group className="mb-2">
               <Form.Label>User Name:</Form.Label>
-              <Form.Control type="text" placeholder={this.state.userName} aria-label="Username" aria-describedby="basic-addon1"/>
+              <Form.Control type="text" name="name" placeholder={this.state.userName} aria-label="Username" aria-describedby="basic-addon1"/>
             </Form.Group>   
 
-            <Form.Group className="mb-2" controlId="formBasicText">
+            <Form.Group className="mb-2">
               <Form.Label>User Surname:</Form.Label>
-              <Form.Control type="text" placeholder={this.state.userSurname} />
+              <Form.Control type="text" name="surName" placeholder={this.state.userSurname} />
             </Form.Group>    
 
-            <Form.Group className="mb-2" controlId="formBasicPassword">
+            <Form.Group className="mb-2">
               <Form.Label>User Password:</Form.Label>
               <Form.Control type="password" placeholder="password" />
-            </Form.Group> 
+            </Form.Group>
 
             <Form.Label>User Sex:</Form.Label>
             <Form.Select className="mb-2" 
+            value={this.state.userSex}
             onChange={(event) => {console.log(event); this.setState({sex: event.target.value})} } 
             aria-label="Form Select Sex">
               <option value="male">Male</option>
@@ -173,56 +174,57 @@ export class User extends Component {
               <option value="other">Other</option>
             </Form.Select>
 
-            <Form.Group as={Col} className="mb-2" controlId="formBasicText">
+            <Form.Group as={Col} className="mb-2">
               <Form.Label>User Birthday:</Form.Label>
-              <Form.Control type="date" placeholder={new Date(this.state.userBirthday.year, this.state.userBirthday.month, this.state.userBirthday.day)} />
+              <Form.Control type="date" 
+              placeholder={new Date(this.state.userBirthday.year, this.state.userBirthday.month, this.state.userBirthday.day)} />
             </Form.Group>
             
-            <Form.Group as={Col} className="mb-2" controlId="formBasicText">
+            <Form.Group as={Col} className="mb-2">
               <Form.Label>Address City:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userAddress.city} />
             </Form.Group>
               
             <Row>
-              <Form.Group as={Col} xs={8} className="mb-2" controlId="formBasicText">
+              <Form.Group as={Col} xs={8} className="mb-2">
                 <Form.Label>Address Street:</Form.Label>
                 <Form.Control type="text" placeholder={this.state.userAddress.street} />
               </Form.Group>
 
-              <Form.Group as={Col} xs={4} className="mb-2" controlId="formBasicText">
+              <Form.Group as={Col} xs={4} className="mb-2">
                 <Form.Label>Number:</Form.Label>
                 <Form.Control type="text" placeholder={this.state.userAddress.number} />
               </Form.Group>
             </Row>
 
-            <Form.Group className="mb-2" controlId="formBasicText">
+            <Form.Group className="mb-2">
               <Form.Label>User Phone Number:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userPhoneNumber} />
             </Form.Group>
             
-            <Form.Group className="mb-2" controlId="formBasicText">
+            <Form.Group className="mb-2">
               <Form.Label>User Email:</Form.Label>
               <Form.Control type="email" placeholder={this.state.userEmail} />
             </Form.Group>
             
-            <Form.Group className="mt-1 mb-2" controlId="formBasicText">
+            <Form.Group className="mt-1 mb-2">
               <Form.Label>Card Name:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userPayment.cardName} />
             </Form.Group>
             
-            <Form.Group className="mt-1 mb-2" controlId="formBasicText">
+            <Form.Group className="mt-1 mb-2">
               <Form.Label>Card Surname:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userPayment.cardSurname} />
             </Form.Group>
             
-            <Form.Group className="mt-1 mb-2" controlId="formBasicText">
+            <Form.Group className="mt-1 mb-2">
               <Form.Label>Card Type:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userPayment.cardType} />
             </Form.Group>
             
             <Row>
               <Form.Label>Card Expiration:</Form.Label>
-              <Col xs={8}>
+              <Col xs={8} className="mr-2">
                 <Form.Select className="mb-2 mr-0" 
                 onChange={(event) => {console.log(event); this.setState({cardExpireMonth: event.target.value})} } 
                 aria-label="Select Card Expiration Month">
@@ -231,8 +233,8 @@ export class User extends Component {
                   ))}
                 </Form.Select>
               </Col>
-              <Col xs={4}>
-                <Form.Select className="mb-2 ml-0" 
+              <Col xs={4} className="ml-0">
+                <Form.Select className="mb-2" 
                 onChange={(event) => {console.log(event); this.setState({cardExpireYear: event.target.value})} }
                 aria-label="Select Card Expiration Year">
                   {this.state.yearList.map( year => (
@@ -242,13 +244,13 @@ export class User extends Component {
               </Col>    
             </Row>
             
-            <Form.Group className="mb-2" controlId="formBasicText">
+            <Form.Group className="mb-2">
               <Form.Label>Card CVV:</Form.Label>
               <Form.Control type="text" placeholder={this.state.userPayment.cardCCV} />
             </Form.Group>
           </fieldset>
           
-          <Button type="button" onClick={this.save} variant={this.state.boolModifying ? "success" : "primary"}>
+          <Button type="submit" value="submit" variant={this.state.boolModifying ? "success" : "primary"}>
             {this.state.boolModifying ? "Salva" : "Modifica"}
           </Button>
 
