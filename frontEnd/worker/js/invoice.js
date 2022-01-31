7010fe980d442b36cf7e28a6f2b86fa19b2beb02
Jean-cube <gianpaolo.roversi@studio.unibo.it>
2022-01-31 12:50:26 +0100

    invoice supreme

--- frontEnd/worker/js/invoice.js
+++ frontEnd/worker/js/invoice.js
@@ -24,27 +24,10 @@ window.onload = function getReservation() {
     });
   }
 
-  function insertData(){
-    $("#ricevente").append(data.clientName + " " + data.clientSurname)
-    $("#emittente").append("NonoNonoPlus")
-    $("#idFattura").append(data._id)
-    $("#dataCrazione").append(data.endDate.day + '/' + data.endDate.month + '/' + data.endDate.year)
-
-    $("#prezzoFisso").append((data.fixedPrice / 122 * 100).toFixed(2))
-    $("#calcoloFisso").append(data.fixedDiscount.onSale ? 
-      (data.fixedDiscount.onSaleType ? data.fixedDiscount.onSaleValue + "%" : data.fixedDiscount.onSaleValue + "€") : 0)
-    $("#imponibileFisso").append(data.fixedDiscount.onSaleType ? 
-      $("#prezzoFisso").val() * $("#calcoloFisso").val() : $("#prezzoFisso").val() - $("#calcoloFisso").val())
-
-    $("#nGiorni").append(calculateDays(data.startDate, data.endDate))
-    $("#prezzoVariabile").append((data.variablePrice / 122 * 100).toFixed(2))
-    $("#calcoloVariabile").append(data.variableDiscount.onSale && calculateDays(data.startDate, data.endDate) > data.variableDiscount.days ?
-      (data.variableDiscount.onSaleType ? data.variableDiscount.onSaleValue + "%" : data.fixedDiscount.onSaleValue + "€") : 0)
-    $("#imponibileVariabile").append($("#prezzoVariabile").val() * $("#calcoloVariabile").val())
-  } 
-
   function calculateDays(obj1, obj2){
     //Create Dates
+    console.log(obj1)
+    console.log(obj2)
     var date1 = new Date(obj1.year, obj1.month-1, obj1.day) 
     var date2 = new Date(obj2.year, obj2.month-1, obj2.day)
 
@@ -56,3 +39,99 @@ window.onload = function getReservation() {
 
     return Difference_In_Days+1
   }
+
+  function scorporo(price) {
+    return price / 122 * 100;
+  }
+  
+  function insertData(data){
+    // Variabili noti
+    userName = data.userName
+    userSurname = data.userSurname
+    userEmail = data.userEmail
+
+    enterpriseName = 'NoloNoloPlus'
+
+    invoiceId = data._id
+    date = data.endDate
+
+    productTitle = data.productTitle
+    productBrand = data.productBrand
+
+    days = calculateDays(data.startDate, data.endDate)
+    console.log(days)
+
+    // Sono i prezzi lordi
+    fixedPrice = parseFloat(data.fixedPrice)
+    variablePrice = parseFloat(data.variablePrice)
+
+    // Sconto di tipo oggetto
+    fixedDiscount = data.fixedDiscount
+    variableDiscount = data.variableDiscount
+
+    // Valori in Prezzo
+    prezzoFissoSenzaIva = scorporo(fixedPrice)
+    prezzoVariabileSenzaIva = scorporo(variablePrice)
+    // Valori in Imponibile
+    imponibileFisso = prezzoFissoSenzaIva
+    if(fixedDiscount.onSale == true) {
+      if(fixedDiscount.onSaleType == true) {
+        imponibileFisso = scorporo(fixedPrice * (100 - parseFloat(fixedDiscount.onSaleValue)) / 100)
+      } else {
+        imponibileFisso = scorporo(fixedPrice) - scorporo(parseFloat(fixedDiscount.onSaleValue))
+      }
+    }
+    imponibileVariabile = prezzoVariabileSenzaIva * days
+    if(variableDiscount.onSale == true && days > variableDiscount.days) {
+      if(variableDiscount.onSaleType == true) {
+        imponibileVariabile = scorporo(variablePrice * days * (100 - parseFloat(variableDiscount.onSaleValue)) / 100)
+      } else {
+        imponibileVariabile = scorporo(variablePrice * days - parseFloat(variableDiscount.onSaleValue)) 
+      }
+    }
+    // Valori in Sconto
+    scontoFisso = 0
+    if(fixedDiscount.onSale == true) {
+      if(fixedDiscount.onSaleType == true) {
+        scontoFisso = parseFloat(fixedDiscount.onSaleValue)
+      } else {
+        scontoFisso = 100 - (imponibileFisso / prezzoFissoSenzaIva * 100)
+      }
+    }
+    scontoVariabile = 0
+    if(variableDiscount.onSale == true && days > variableDiscount.days) {
+      if(variableDiscount.onSaleType == true) {
+        scontoVariabile = parseFloat(variableDiscount.onSaleValue)
+      } else {
+        scontoVariabile = 100 - (imponibileVariabile / days / prezzoVariabileSenzaIva * 100)
+      }
+    }
+
+    var totale = imponibileFisso + imponibileVariabile
+    var penale = 0
+    if(new Date() > new Date(data.endDate.year, data.endDate.month - 1, data.endDate.day) && data.isReturned == false && data.isTaken == true){
+       penale = (imponibileFisso + imponibileVariabile) * 1.22 * 0.1
+     }
+
+    //Inserimento campi
+    $("#ricevente").append(data.clientName + " " + data.clientSurname + " - " + data.clientEmail)
+    $("#emittente").append("NonoNonoPlus")
+    $("#idFattura").append(data._id)
+    $("#dataCrazione").append(data.endDate.day + '/' + data.endDate.month + '/' + data.endDate.year)
+    
+    $("#descrizioneFisso").append(data.productTitle + " " + data.productBrand + " (fisso)")
+    $("#descrizioneVariabile").append(data.productTitle + " " + data.productBrand + " (variabile)")
+    $("#nGiorni").append(days)
+    $("#prezzoFisso").append((prezzoFissoSenzaIva).toFixed(2) + " €")
+    $("#prezzoVariabile").append((prezzoVariabileSenzaIva).toFixed(2) + " €")
+    $("#scontoFisso").append((scontoFisso).toFixed(2) + "%")
+    $("#scontoVariabile").append((scontoVariabile).toFixed(2) + "%")
+    $("#imponibileFisso").append((imponibileFisso).toFixed(2) + " €")
+    $("#imponibileVariabile").append((imponibileVariabile).toFixed(2) + " €")
+    
+    $("#totaleImponibile").append((totale).toFixed(2) + " €")
+    $("#totaleIVA").append((totale * 0.22).toFixed(2) + " €")
+    $("#penale").append((penale).toFixed(2) + " €")
+    $("#totale").append((totale + totale * 0.22 + penale).toFixed(2) + " €")
+    $("#note").append(data.description)    
+  } 
